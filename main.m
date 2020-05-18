a065cc25703faba56c3fc2037f6e1d473105c88e
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-18 17:19:00 +0100

    Running in main

--- main.m
+++ main.m
@@ -1,1 +1,17 @@
 % Most of the content happens here
+
+%% Setup
+% Mobile Robot
+initial_pose = [2,2,0];     % [x, y, theta]
+
+% Map
+load exampleMap;
+occupancy_matrix = occupancyMatrix(map);
+
+%% Compute Search Path
+search_path = [2,2;
+               3,3;
+               3,5];
+
+%% Simulate Search
+result = simulate_static_search(initial_pose, search_path);
