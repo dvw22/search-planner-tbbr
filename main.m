ffb2c29a0864b03390076f360046acc4a2d0bdaf
danielvanwensveen <danielvanwensveen@daniels-air-4.home>
2020-05-18 15:18:00 +0100

    Create main and first function

---
+++ main.m
@@ -0,0 +1,1 @@
+% Most of the content happens here
---
+++ static_search.m
@@ -0,0 +1,6 @@
+function [search_result] = static_search(search_path, map, mobility_controller)
+%static_search Makes a mobile robot execute a search path on the map.
+%   Detailed explanation goes here
+
+end
+
