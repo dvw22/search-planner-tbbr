c9256ec99b94e8f4b3e8790835b70234492165d0
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-21 13:34:18 +0100

    Added slice connectivity checking function

--- slice_adjacency.m
+++ slice_adjacency.m
@@ -1,15 +1,15 @@
-function [adjacency_matrix] = slice_adjacency(con_seg_L,con_seg_R)
-%slice_adjacency Returns the adjacency matrix for two slices with segmented
-%sections
+function [adjacency_matrix] = connections_adjacency(connections_L,connections_R)
+% connections_adjacency Returns the adjacency matrix for two neighbouring
+% connectivity slices
 %   Detailed explanation goes here
 
 %% Initialise Matrix
-adjacency_matrix = zeros(length(con_seg_L), length(con_seg_R)); 
+adjacency_matrix = zeros(length(connections_L), length(connections_R)); 
 
 %% Looping
 for i = 1:length(con_seg_L)
     for j = 1:length(con_seg_R)
-        if min(con_seg_L(i,2), con_seg_R(j,2)) - max(con_seg_L(i,1), con_seg_R(j,1)) > 0
+        if min(connections_L(i,2), connections_R(j,2)) - max(connections_L(i,1), connections_R(j,1)) > 0
             adjacency_matrix(i, j) = 1;
         end
     end
