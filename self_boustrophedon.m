fb9ff8ffd50feff2091e30e49a217ff75dbe01ca
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-22 00:31:44 +0100

    Code working so far with good commenting

--- self_boustrophedon.m
+++ self_boustrophedon.m
@@ -24,14 +24,36 @@ for col = 1:size(map,2)
     [connectivity, connections] = slice_connectivity(map(:,col));
     
     %% Compare slices using adjacency matrix
-    
-    adjacency_matrix = connections_adjacency(last_connections,connections);
+    adj_matrix = connections_adjacency(last_connections,connections);
      
     %% Determine slice splitting/joining
-    if connectivity > last_connectivity
-        % New cell(s) has/have been formed
+    % Each individual connection in the slice can be checked for splitting
+    % or joining. This is done using the adjacency matrix. The sum of a row
+    % indicates how many right slice connections a left slice connections
+    % is adjacent to. The sum of a column indicates how many left slice
+    % connections a right slice connection is adjacent to.
+    
+    % Compare slices left to right using adjacency matrix row
+    for i = 1:size(adj_matrix,1)
+        % The connection does not split
+        if sum(adj_matrix(i,:)) == 1
+            sum(adj_matrix(i,:))
+        % The connection split: IN condition
+        elseif sum(adj_matrix(i,:)) > 1
+            sum(adj_matrix(i,:))
+        end
     end
     
+    % Compare slices right to left using adjacency matrix columns
+    for i = 1:size(adj_matrix,2)
+        % The connection joined: OUT condition
+        if sum(adj_matrix(:,i)) > 1
+            sum(adj_matrix(:,i))
+        % A new connection formed inside an obstacle: IN condition
+        elseif sum(adj_matrix(:,i)) == 0
+            sum(adj_matrix(:,i))
+        end
+    end
     
     % Update map
     % No connectivity means a full line of obstacles
