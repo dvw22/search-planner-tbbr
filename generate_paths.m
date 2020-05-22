0199045ac02937178fc8ca1c3493aaa17077ad8a
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-22 18:47:03 +0100

    Started writing path generating function

--- generate_paths.m
+++ generate_paths.m
@@ -2,7 +2,13 @@ function [cell_paths] = generate_paths(decomposed_map)
 % generate_paths Generates boustrophedon paths in each cell in a
 % boustrophedon cell decomposed occupancy map
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+
+cell = 1;
+
+% Zero all other cells
+zeroed_except_cell = decomposed_map==cell;
+col_cut = zeroed_except_cell(:,any(zeroed_except_cell,1));  % cut zero columns
+truncated_cell = col_cut(any(col_cut,2),:);  % cut zero rows
+
+
 
