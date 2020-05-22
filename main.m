0f24537ef1f8eec057dd5fa48b7535fb2fcc650e
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-23 00:53:46 +0100

    Set up main for testing, scaling issues for waypoints

--- main.m
+++ main.m
@@ -6,14 +6,22 @@ initial_pose = [2,2,0];     % [x, y, theta]
 
 % Map
 load exampleMap;
-occupancy_matrix = occupancyMatrix(map);
-[area, num_cells] = unoccupied_area(map);
+occ_map = occupancyMatrix(map);
+bi_occ_map = round(occ_map);  % convert to binary
+% [area, num_cells] = unoccupied_area(map);
 
 %% Compute Search Path
-search_path = [2.5,2;
-               3,3;
-               1,5;
-               3,6];
+% Perform cell decomposition on map
+[decomposed_map, num_cells] = btd_cell_decomposition(bi_occ_map);
+display_decomposed_map(decomposed_map)
+
+% Get boustrophedon waypoints for cell
+search_path = cell_search_path(decomposed_map, 5);
+
+% search_path = [2.5,2;
+%                3,3;
+%                1,5;
+%                3,6];
 
 %% Simulate Search
 result = simulate_static_search(initial_pose, search_path);
