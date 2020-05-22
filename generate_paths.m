3297003ce853d49d986d96c32c7c4df56a04ea17
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-22 20:47:09 +0100

    Added offset calculations to get true indices

--- generate_paths.m
+++ generate_paths.m
@@ -10,15 +10,23 @@ zeroed_except_cell = decomposed_map==cell;  % zero all other cells
 col_cut = zeroed_except_cell(:,any(zeroed_except_cell,1));  % cut zero columns
 truncated_cell = col_cut(any(col_cut,2),:);  % cut zero rows
 
-% Find cell length
+% Find cell length and height
 cell_length = size(truncated_cell,2);
+cell_height = size(truncated_cell,1);
 
 % Initialise ceiling and floor arrays to track indices
 cell_ceiling_idx = zeros(cell_length,1);  % [x1, x2, x3, ...]
 cell_floor_idx = cell_ceiling_idx;
-ceiling_offset = 0;  % how many zero rows were cut from the top
-floor_offset = 0;  % how many zero rows were cut from the bottom
 
+% Find ceiling offset of cell
+for i = 1:size(zeroed_except_cell,1)
+    if ~all(zeroed_except_cell(i,:)==0)
+        ceiling_offset = i-1;
+        break
+    end
+end
+
+% Look for ceiling and floor indices within cell
 for col = 1:cell_length
     % Look down through each row until top of cell is found in column slice
     for i = 1:size(truncated_cell,1)
@@ -32,7 +40,7 @@ for col = 1:cell_length
     % slice
     for i = size(truncated_cell,1):-1:1
         if truncated_cell(i,col)==cell
-            cell_floor_idx(col,1) = i + floor_offset;  % store x index
+            cell_floor_idx(col,1) = i + ceiling_offset;  % store x index
             break
         end
     end
