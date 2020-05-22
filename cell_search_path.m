1f6b79757cbf8cedd90b7163736f2d363e38b9da
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-22 23:28:36 +0100

    started creating boustrophedon paths

---
+++ cell_search_path.m
@@ -0,0 +1,8 @@
+function [waypoints] = cell_search_path(decomposed_map,cell)
+% cell_search path Generates a search path within a cell
+%   Detailed explanation goes here
+
+[ceiling_idx, floor_idx] = ceil_floor_cell(decomposed_map, cell);
+
+end
+
