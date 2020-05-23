295ef5a554645201578d582b5ed6f5a94f918638
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-23 19:50:07 +0100

    Waypoints are now available on a cell by cell basis

--- main.m
+++ main.m
@@ -6,8 +6,8 @@ initial_pose = [0.75,0.75,pi/2];  % [x, y, theta]
 opi = [8, 8, 1];  % [x, y, label]
 
 % Map
-load exampleMap;
-% load complexMap
+% load exampleMap;
+load complexMap;
 occ_map = occupancyMatrix(map);
 bi_occ_map = round(occ_map);  % convert to binary
 resolution = map.Resolution;
@@ -26,11 +26,4 @@ resolution = map.Resolution;
 display_decomposed_map(decomposed_map)
 
 %% Simulate Search
-for cell = 1:num_cells
-    % Get boustrophedon waypoints for cell
-    search_path = cell_search_path(decomposed_map, cell, resolution);
-    
-    % Simulate
-    result = simulate_static_search(initial_pose, search_path, opi);
-end
-% search_path = cell_search_path(decomposed_map, 10, resolution);
+result = simulate_static_search(initial_pose, opi, decomposed_map, resolution, num_cells);
