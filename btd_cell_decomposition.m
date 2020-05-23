8bcfe56b36adc4b83ed63713cb140a63a8810941
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-23 01:58:40 +0100

    Code is completely reversed now so lidar map won't work, but unit tests work

--- btd_cell_decomposition.m
+++ btd_cell_decomposition.m
@@ -66,9 +66,8 @@ for col = 1:size(occupancy_map,2)
             % The connection does not split or join (dead end)
             elseif sum(adj_matrix(i,:)) == 0
                 % Remove the cell from the current cells array
-                changes = changes - 1;  % a cell has been removed
-                before_removal = current_cells(1:(i+changes)-1);
-                after_removal = current_cells((i+changes)+1:size(current_cells,2));
+                before_removal = current_cells(1:i-1);
+                after_removal = current_cells(i+1:size(current_cells,2));
                 current_cells = [before_removal, after_removal];
                 
             end
