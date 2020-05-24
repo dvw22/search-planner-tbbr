663b1d3d1abb083e1060d7b229ef22368cd0343b
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-24 19:19:12 +0100

    Done, much more modular now. Beautiful.

--- map_search_path.m
+++ map_search_path.m
@@ -10,33 +10,52 @@ map_waypoints = [];
 segment_idx = [];
 num_cell_seq = size(cell_order,1);
 
+last_end_idx = 0;
+start_idx = 1;
+
 % Get waypoints for each cell sequence and path between and append
 for i = 1:2:num_cell_seq
     % Append cell sequence waypoints
     [cell_seq_waypoints, num_cells] = cell_seq_search_path(cell_order(i,:),decomposed_map,resolution);
     map_waypoints = [map_waypoints; cell_seq_waypoints];
     
-    % Save indices
+    % Append cell sequence indices
+    num_waypoints = size(cell_seq_waypoints,1);
+    end_idx = last_end_idx + num_waypoints;
+    segment_idx = [segment_idx; [start_idx, end_idx]];
+    start_idx = end_idx + 1;  % store
+    last_end_idx = end_idx;  % store
     
-    % Append travel waypoints
+    % Append travel and next cell sequence waypoints
     if i < num_cell_seq
         % Get next cell sequence waypoints
         [next_cell_seq_waypoints, num_cells] = cell_seq_search_path(cell_order(i+1,:),decomposed_map,resolution);
         
         % Get start and end points for travel
-        start_point = cell_seq_waypoints(end,:);  % starting at end of last cell sequence
-        end_point = next_cell_seq_waypoints(1,:);  % ending at start of next cell sequence
+        start_waypoint = cell_seq_waypoints(end,:);  % starting at end of last cell sequence
+        end_waypoint = next_cell_seq_waypoints(1,:);  % ending at start of next cell sequence
         
         % Append travel waypoints
-        travel_waypoints = findpath(planner,start_point,end_point);
+        travel_waypoints = findpath(planner,start_waypoint,end_waypoint);
         map_waypoints = [map_waypoints; travel_waypoints];
         
-        % Save indices
+        % Append travel indices
+        num_waypoints = size(travel_waypoints,1);
+        end_idx = last_end_idx + num_waypoints;
+        segment_idx = [segment_idx; [start_idx, end_idx]];
+        start_idx = end_idx + 1;  % store
+        last_end_idx = end_idx;  % store
         
         % Append next cell sequence waypoints
         map_waypoints = [map_waypoints; cell_seq_waypoints];
     
-        % Save indices
+        % Append next cell sequence indices
+        num_waypoints = size(next_cell_seq_waypoints,1);
+        end_idx = last_end_idx + num_waypoints;
+        segment_idx = [segment_idx; [start_idx, end_idx]];
+        start_idx = end_idx + 1;  % store
+        last_end_idx = end_idx;  % store
+        
     end
     
 
