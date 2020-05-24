3cdb8866cd98831ed84078eeeb4ebf8e7b28459e
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-24 03:29:05 +0100

    Very jank code that almost works

--- plan_cell_order.m
+++ plan_cell_order.m
@@ -4,37 +4,38 @@ function [cell_order] = plan_cell_order(decomposed_map, reeb_graph, num_cells)
 %   Detailed explanation goes here
 
 % Initialise
-cell_order = zeros(num_cells);
+cell_order = zeros(num_cells,1);
+unsearched_cells = [1:num_cells];
 
 % Get starting cell number
 start = 1;
-cell_order(1) = start;
+cell = start;
+cell_order(1) = cell;
+unsearched_cells(start) = [];  % remove start cell
 
-for cell_order_idx = 1:num_cells
+for cell_order_idx = 2:num_cells
     % Initialise
     unsearched_neighbor = false;
     
-    % Get adjacent cells
-    adj_cells = neighbors(reeb_graph,cell);
-    
-    if 
-    
-    % Try find a neighbour that hasn't been visited
-    for i = 1:size(adj_cells,1)
-        % Cell hasn't been visited
-        if ismember(adj_cells(i),cell_order) == 0
-            unsearched_neighbor = true;
-            cell = adj_cells(i);
-            cell_order(list_idx) = cell;
-            break  % greedy
-        end
+    % Check if there are next cells
+    if outdegree(reeb_graph,cell) > 0
+        % Store next cell
+        next_cells = successors(reeb_graph,cell);
+        cell = next_cells(1);  % ASSUMPTION: TAKE THE FIRST ALWAYS
+        
+        % Update
+        unsearched_neighbor = true;
+        unsearched_cells(find(unsearched_cells==cell)) = [];  % remove cell
+        cell_order(cell_order_idx) = cell;
     end
     
     % No unsearched neighbours found. Move to lowest unsearched cell
     if unsearched_neighbor == false
-        
+        % Add lowest unsearched cell
+        cell = min(unsearched_cells);
+        unsearched_cells(find(unsearched_cells==cell)) = [];  % remove cell
+        cell_order(cell_order_idx) = cell;
     end
-    
     
 end
 
