4a42aefac7bd7fd717ef44f0cdaa0c57583f1547
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-24 23:51:40 +0100

    Complete

--- main.m
+++ main.m
@@ -21,14 +21,18 @@ bi_occ_map = binaryOccupancyMap(bi_occ_matrix,resolution);
 start_pose = [0.75,0.75,pi/2];  % [x, y, theta]
 start_position = [start_pose(1), start_pose(2)];  % [x, y]
 opi = [8, 8, 1];  % [x, y, label]
+
+% Probabilistic Roadmap
 planner = mobileRobotPRM(bi_occ_map);
+planner.NumNodes = 75;
+planner.ConnectionDistance = 5;
 
 % [area, num_cells] = unoccupied_area(map);
 
 %% Decompose Map
 % Perform cell decomposition on map
 [decomposed_map, graph, num_cells] = btd_cell_decomposition(bi_occ_matrix);
-display_decomposed_map(decomposed_map)
+% display_decomposed_map(decomposed_map)
 
 %% Plan Cell Order
 cell_order = plan_cell_order(graph, num_cells);
@@ -37,7 +41,14 @@ cell_order = plan_cell_order(graph, num_cells);
 [map_waypoints, segment_idx] = map_search_path(cell_order,decomposed_map,resolution,planner,start_position);
 
 %% Plot Path
+% Simulation Visualiser
+viz = Visualizer2D;
+viz.hasWaypoints = true;
+viz.mapName = 'map';
+viz(start_pose,map_waypoints)
+hold on
 plot(map_waypoints(:,1),map_waypoints(:,2),'-x')
+hold off
 
 %% Simulate Search
 % result = simulate_static_search(start_pose, opi, map_waypoints, segment_idx);
