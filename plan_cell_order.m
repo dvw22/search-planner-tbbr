a8bc06364f899218b96c3e8ed18aa553cbedf620
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-24 02:03:20 +0100

    Added new function

---
+++ plan_cell_order.m
@@ -0,0 +1,7 @@
+function [cell_order] = plan_cell_order(decomposed_map, reeb_graph, num_cells)
+% plan_cell_order Determines what order the cells should be searched for
+% complete coverage
+%   Detailed explanation goes here
+
+end
+
