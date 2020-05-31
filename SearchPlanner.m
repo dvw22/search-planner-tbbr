6d53b0b90876fc194d2fef50f23f53abb06c37be
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-31 19:43:14 +0100

    Fixed method calls and added search_path method

--- SearchPlanner.m
+++ SearchPlanner.m
@@ -3,30 +3,143 @@ classdef SearchPlanner
     %   Detailed explanation goes here
     
     properties
+        map_resolution
         decomposed_matrix
         graph
         num_cells
         cell_order
+        PathPlanner
     end
     
     methods
         function obj = SearchPlanner(occ_map)
             %SearchPlanner Construct an instance of this class
             
-            % Process map
+            % Convert map to binary map
+            obj.map_resolution = occ_map.Resolution;
             occ_matrix = occupancyMatrix(occ_map);
-            bi_occ_matrix = round(occ_matrix);  % convert to binary
+            bi_occ_matrix = round(occ_matrix);  % convert to binary matrix
+            bi_occ_map = binaryOccupancyMap(bi_occ_matrix,obj.map_resolution);
             
             % Perform cell decomposition of map
-            [obj.decomposed_matrix, obj.graph, obj.num_cells] = btd_cell_decomposition(bi_occ_matrix);
+            [obj.decomposed_matrix, obj.graph, obj.num_cells] = obj.btd_cell_decomposition(bi_occ_matrix);
             
             % Perform cell order calculation
-            obj.cell_order = plan_cell_order(obj.graph, obj.num_cells);
+            obj.cell_order = obj.plan_cell_order(obj.graph);
+            
+            % Initialise shortest path planner
+            obj.PathPlanner = mobileRobotPRM(bi_occ_map);
+            obj.PathPlanner.NumNodes = 75;
+            obj.PathPlanner.ConnectionDistance = 5;
         end
-    end
-    
-    methods (Access = private)
-        function [decomposed_matrix, reeb_graph, cell_counter] = btd_cell_decomposition(bi_occ_matrix)
+        
+        function [map_waypoints,segment_idx] = map_search_path(obj,init_pos)
+            % map_search_path Outputs a full waypoint list with a segment indices matrix
+            % to access segmented regions during the waypoint planning process.
+            %   The segments are either a cell sequence path or a shortest path between
+            %   cells.
+
+            %% Waypoint Generation
+            % Initialise
+            map_waypoints = [];
+            segment_idx = [];
+            num_cell_seq = size(obj.cell_order,1);
+            original_num_nodes = obj.PathPlanner.NumNodes;
+            original_conn_dis = obj.PathPlanner.ConnectionDistance;
+
+            last_end_idx = 0;
+            start_idx = 1;
+
+            % Get waypoints for each cell sequence and path between and append
+            for i = 1:num_cell_seq
+                % Calculate waypoints before travel first time only
+                if i == 1
+                    % Append cell sequence waypoints
+                    [cell_seq_waypoints, num_cells] = cell_seq_search_path(obj.cell_order(i,:),obj.decomposed_matrix,obj.map_resolution,obj.PathPlanner);
+                    map_waypoints = [map_waypoints; cell_seq_waypoints];
+
+                    % Append cell sequence indices
+                    num_waypoints = size(cell_seq_waypoints,1);
+                    end_idx = last_end_idx + num_waypoints;
+                    segment_idx = [segment_idx; [start_idx, end_idx]];
+                    start_idx = end_idx + 1;  % store
+                    last_end_idx = end_idx;  % store
+
+                    % Plan path to first waypoint
+                    travel_waypoints = findpath(obj.PathPlanner,init_pos,map_waypoints(1,:));
+                    % The planner may not always find a path if the map is too complex.
+                    % This condition is to prevent the code from breaking.
+                    if isempty(travel_waypoints) == 0
+                        travel_waypoints(1,:) = [];  % Must clear source
+                        travel_waypoints(end,:) = [];  % Must clear destination of travel to avoid duplicate
+                    else
+                        display(['Path planning failed between cell sequence ',num2str(i),' and ',num2str(i+1),'.'])
+                    end
+                    map_waypoints = [travel_waypoints; map_waypoints];
+
+                    % Append travel indices
+                    num_waypoints = size(travel_waypoints,1);
+                    end_idx = last_end_idx + num_waypoints;
+                    segment_idx = [segment_idx; [start_idx, end_idx]];
+                    start_idx = end_idx + 1;  % store
+                    last_end_idx = end_idx;  % store
+
+                % Just finished calculating a travel segment
+                else
+                    % Append cell sequence waypoints
+                    map_waypoints = [map_waypoints; cell_seq_waypoints];
+
+                    % Append cell sequence indices
+                    num_waypoints = size(cell_seq_waypoints,1);
+                    end_idx = last_end_idx + num_waypoints;
+                    segment_idx = [segment_idx; [start_idx, end_idx]];
+                    start_idx = end_idx + 1;  % store
+                    last_end_idx = end_idx;  % store
+                end
+
+                % Append travel waypoints
+                if i < num_cell_seq
+                    % Get next cell sequence waypoints
+                    [next_cell_seq_waypoints, num_cells] = cell_seq_search_path(obj.cell_order(i+1,:),obj.decomposed_matrix,obj.map_resolution,obj.PathPlanner);
+
+                    % Get start and end points for travel
+                    start_waypoint = cell_seq_waypoints(end,:);  % starting at end of last cell sequence
+                    end_waypoint = next_cell_seq_waypoints(1,:);  % ending at start of next cell sequence
+
+                    % Append travel waypoints
+                    travel_waypoints = findpath(obj.PathPlanner,start_waypoint,end_waypoint);
+                    % The planner may not always find a path if the map is too complex.
+                    % This condition is to prevent the code from breaking.
+                    while isempty(travel_waypoints) == 1
+                        % Publish info
+                        display(['Path planning failed between cell sequence ',num2str(i),' and ',num2str(i+1),'.'])
+                        display('Increasing nodes and connection distance.')
+
+                        % Increase nodes and connection distances
+                        obj.PathPlanner.NumNodes = obj.PathPlanner.NumNodes * 2;
+                        obj.PathPlanner.ConnectionDistance = obj.PathPlanner.ConnectionDistance * 2;
+
+                        % Calculate again
+                        travel_waypoints = findpath(obj.PathPlanner,start_waypoint,end_waypoint);
+                    end    
+                    travel_waypoints(1,:) = [];  % Must clear source of travel to avoid duplicate
+                    travel_waypoints(end,:) = [];  % Must clear destination of travel to avoid duplicate
+                    map_waypoints = [map_waypoints; travel_waypoints];
+
+                    % Append travel indices
+                    num_waypoints = size(travel_waypoints,1);
+                    end_idx = last_end_idx + num_waypoints;
+                    segment_idx = [segment_idx; [start_idx, end_idx]];
+                    start_idx = end_idx + 1;  % store
+                    last_end_idx = end_idx;  % store
+
+                    % Store next cell sequence waypoints
+                    cell_seq_waypoints = next_cell_seq_waypoints;
+                end
+            end
+        end
+
+        function [decomposed_matrix, reeb_graph, cell_counter] = btd_cell_decomposition(obj,bi_occ_matrix)
             %btd_cell_decomposition Uses boustrophedon cell decomposition to output a
             %region divided occupancy map
             %   The occupancy map must have a border of occupied cells for it to work.
@@ -190,7 +303,7 @@ classdef SearchPlanner
             end
         end
         
-        function [cell_order] = plan_cell_order(reeb_graph)
+        function [cell_order] = plan_cell_order(obj,reeb_graph)
             % plan_cell_order Determines what order the cells should be searched for
             % complete coverage
             %   Detailed explanation goes here
@@ -282,7 +395,6 @@ classdef SearchPlanner
                 end
             end
         end
-
 
     end
 end
