b06b54e3e272f14bfd9896171cc63a4aedd0177f
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-31 20:53:29 +0100

    Added more methods and deleted redundant functions

--- SearchPlanner.m
+++ SearchPlanner.m
@@ -160,7 +160,7 @@ classdef SearchPlanner
             for col = 1:size(bi_occ_matrix,2)
                 %% Check connectivity of the slice
                 slice = bi_occ_matrix(:,col);
-                [connectivity, connections] = slice_connectivity(slice);
+                [connectivity, connections] = obj.slice_connectivity(slice);
 
                 %% 1. Check if we are coming out of a full obstacle slice
                 if last_connectivity == 0
@@ -191,7 +191,7 @@ classdef SearchPlanner
                 else
                     %% 3. Connection(s) present in current or last slice
                     % Compare slices using adjacency matrix
-                    adj_matrix = connections_adjacency(last_connections,connections);
+                    adj_matrix = obj.connections_adjacency(last_connections,connections);
 
                     % Compare slices left to right using adjacency matrix row
                     for i = 1:size(adj_matrix,1)
@@ -304,6 +304,68 @@ classdef SearchPlanner
                 last_cells = current_cells;
             end
         end
+        
+        function [connectivity,connections] = slice_connectivity(obj,slice)
+            % slice_connectivity Calculates the number of connected segments and
+            % returns their indices
+            %   If the previous index is a 0 and this one is a 1, a connectivity has
+            %   opened. Log this point as a start point. Otherwise, if the previous 
+            %   index is a 1 and this one is a 0 and a connectivity was opened, a 
+            %   connectivity has closed. Log this point as an end point. 
+            %   Try to track the number of connectivities. The start and end of a
+            %   connectivity should occupy a row.
+
+            %% Initialise
+            connectivity = 0;
+            last_data = 0;
+            open_part = false;
+            connections = []; % unknown length
+
+            %% Loop Through
+            for i = 1:length(slice)
+                data = slice(i);
+
+                % Connectivity opens
+                if data == 0 && open_part == false
+                    open_part = true;
+                    connectivity = connectivity + 1;
+                    start_point = i;
+                % Connectivity closes
+                elseif last_data == 0 && data == 1 && open_part == true
+                    open_part = false;
+                    end_point = i;
+                    connections(connectivity, 1) = start_point;
+                    connections(connectivity, 2) = end_point;
+                % Logs the part closing if it reaches the end of the slice
+                elseif i == length(slice) && open_part == true
+                    open_part = false;
+                    end_point = i;
+                    connections(connectivity, 1) = start_point;
+                    connections(connectivity, 2) = end_point;
+                end
+
+                % Store data
+                last_data = data;
+            end
+        end
+        
+        function [adjacency_matrix] = connections_adjacency(obj,connections_L,connections_R)
+            % connections_adjacency Returns the adjacency matrix for two neighbouring
+            % connectivity slices
+            %   Detailed explanation goes here
+
+            %% Initialise Matrix
+            adjacency_matrix = zeros(size(connections_L,1), size(connections_R,1)); 
+
+            %% Looping
+            for i = 1:size(connections_L,1)
+                for j = 1:size(connections_R,1)
+                    if min(connections_L(i,2), connections_R(j,2)) - max(connections_L(i,1), connections_R(j,1)) > 0
+                        adjacency_matrix(i, j) = 1;
+                    end
+                end
+            end
+        end 
         
         function [cell_order] = plan_cell_order(obj,reeb_graph)
             % plan_cell_order Determines what order the cells should be searched for
