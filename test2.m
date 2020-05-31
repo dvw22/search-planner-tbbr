0c8f5894186d95b394f18d7dddaa00bd66ef7c38
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-31 14:14:34 +0100

    Almost there, need to debug edge case with obstacles

--- test2.m
+++ test2.m
@@ -1,3 +1,5 @@
+clear()
+
 % Occupancy matrix from map
 load exampleMap;
 % load complexMap;
@@ -9,15 +11,15 @@ resolution = map.Resolution;
 bi_occ_map = binaryOccupancyMap(bi_occ_matrix,resolution);
 
 % Lidar
+% Homemade
+scan_angles = linspace(-pi/6,pi/6,12);
+max_range = 5;
+ranges = zeros(size(scan_angles,2),1);
 % Object
 area_measure = LidarSensor;
 area_measure.sensorOffset = [0,0];
-area_measure.scanAngles = linspace(-pi/6,pi/6,6);
-area_measure.maxRange = 5;
-% Homemade
-scan_angles = linspace(-pi/6,pi/6,6);
-max_range = 5;
-ranges = zeros(size(scan_angles,2),1);
+area_measure.scanAngles = scan_angles;
+area_measure.maxRange = max_range;
 
 % Pose
 initPose = [1; 3; pi/4];        % Initial pose (x y theta)
@@ -37,7 +39,7 @@ ranges_xy = rayIntersection(map,pose,scan_angles,max_range);
 % Convert to distance between
 for i = 1:size(ranges,1)
     if isnan(ranges_xy(i,1))
-        ranges(i) = 5;
+        ranges(i) = max_range;
     else
         dist_between = [pose(1,1),pose(2,1);ranges_xy(i,1),ranges_xy(i,2)];
         ranges(i) = pdist(dist_between,'euclidean');
@@ -47,16 +49,24 @@ end
 % Update search map
 for i = 1:size(ranges,1)
     % Check for NaN
-    if isnan(ranges(i))
+    if ranges(i) == max_range 
         [end_unit, grid_units] = raycast(map,pose,area_measure.maxRange,area_measure.scanAngles(i));
+        
+        % Convert midpoints to linear indices
+        row_idx = [grid_units(:,1); 0];
+        col_idx = [grid_units(:,2); 0];
+        % Add endpoint too
+        row_idx(end) = end_unit(1,1);
+        col_idx(end) = end_unit(1,2);
+        linear_idx = sub2ind(size_map,row_idx,col_idx);
     else
         [end_unit, grid_units] = raycast(map,pose,ranges(i),scan_angles(i));
+        
+        % Convert midpoints to linear indices
+        row_idx = grid_units(:,1);
+        col_idx = grid_units(:,2);
+        linear_idx = sub2ind(size_map,row_idx,col_idx);
     end
-
-    % Convert to linear indices
-    row_idx = grid_units(:,1);
-    col_idx = grid_units(:,2);
-    linear_idx = sub2ind(size_map,row_idx,col_idx);
 
     % Replace raycast indices with 2 to indicate searched
     search_matrix(linear_idx) = 2;
