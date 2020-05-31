ca3f90c0b73b819ed07a4a208f634c7b1ef8d86d
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-05-31 18:59:51 +0100

    Added cell order method

--- SearchPlanner.m
+++ SearchPlanner.m
@@ -21,6 +21,7 @@ classdef SearchPlanner
             [obj.decomposed_matrix, obj.graph, obj.num_cells] = btd_cell_decomposition(bi_occ_matrix);
             
             % Perform cell order calculation
+            obj.cell_order = plan_cell_order(obj.graph, obj.num_cells);
         end
     end
     
@@ -186,11 +187,103 @@ classdef SearchPlanner
                 last_connectivity = connectivity;
                 last_connections = connections;
                 last_cells = current_cells;
-                last_slice = slice;
-
             end
-
         end
+        
+        function [cell_order] = plan_cell_order(reeb_graph)
+            % plan_cell_order Determines what order the cells should be searched for
+            % complete coverage
+            %   Detailed explanation goes here
+
+            % Initialise arrays and counters
+            cell_order = [];
+            cell_seq = zeros(1,numnodes(reeb_graph));
+            unsearched_cells = (1:numnodes(reeb_graph));
+            cell_order_idx = 1;
+            cell_seq_idx = 2;
+
+            % Get starting cell number and add to first sequence
+            start = 1;
+            cell = start;
+            cell_seq(1) = start;
+            unsearched_cells(start) = [];  % remove start cell
+
+            % Continue while there are still unsearched cells
+            while true
+                % Assume all successor cells have been searched
+                all_searched = true;
+
+                % The cell is not a dead end
+                if outdegree(reeb_graph,cell) > 0
+                    % Check successors cells
+                    next_cells = successors(reeb_graph,cell);
+
+                    % Check if there is an unsearched successor
+                    for i = 1:size(next_cells,2)
+                        % A successor hasn't been searched yet
+                        if find(unsearched_cells==next_cells(i))
+                            % Update flag
+                            all_searched = false;
+
+                            % Update arrays
+                            cell = next_cells(i);
+                            cell_seq(cell_seq_idx) = cell;  % append to continuous cell sequence
+
+                            % New cell is no longer unsearched
+                            unsearched_cells(find(unsearched_cells==cell)) = [];  % remove cell
+                            break
+                        end
+                    end
+
+                    % All the successors have already been searched
+                    if all_searched == true
+                        % Add sequence to cell order
+                        cell_order = [cell_order; cell_seq];  % add to cell order
+                        cell_seq(1,:) = 0;  % clear sequence
+
+                        % Update indices
+                        cell_order_idx = cell_order_idx + 1;
+                        cell_seq_idx = 1;
+
+                        % Add lowest unsearched cell
+                        cell = min(unsearched_cells);
+                        cell_seq(cell_seq_idx) = cell;
+
+                        % New cell is no longer unsearched
+                        unsearched_cells(find(unsearched_cells==cell)) = [];  % remove cell
+                    end     
+
+                % The cell is a dead end
+                else
+                    % Add sequence to cell order
+                    cell_order = [cell_order; cell_seq];
+                    cell_order_idx = cell_order_idx + 1;
+
+                    % Reset cell sequence
+                    cell_seq(1,:) = 0;  % clear sequence
+                    cell_seq_idx = 1;  % restart index
+
+                    % Add lowest unsearched cell
+                    cell = min(unsearched_cells);
+                    cell_seq(cell_seq_idx) = cell;
+
+                    % New cell is no longer unsearched
+                    unsearched_cells(find(unsearched_cells==cell)) = [];  % remove cell
+                end
+
+                % Increment loop
+                cell_seq_idx = cell_seq_idx + 1;
+
+                % Escape condition
+                if isempty(unsearched_cells) == 1
+                    % Add last cell sequence
+                    cell_order = [cell_order; cell_seq];
+                    break
+                end
+            end
+        end
+
+
     end
 end
 
