3b19607e268bc0477ff2fbe28610dd9d17e2006b
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-06-01 13:52:05 +0100

    Deleted redundant functions

--- searched_area.m
+++
@@ -1,8 +0,0 @@
-function [searched_area,num_searched_cells] = searched_area(search_matrix,map)
-%unoccupied_area Calculates the total searched area in the map
-%   Detailed explanation
-
-num_searched_cells = sum(sum(search_matrix));
-searched_area = (1/map.Resolution)^2 * num_searched_cells;    % convert to area
-end
-
