b0c7cd7e1c2baf15833dea45ab4093946bf1f9a1
Daniel van Wensveen <dvw22@bath.ac.uk>
2020-06-02 15:53:21 +0100

    Hard map added but fails. There is an issue with cell decomposition. Says 18 should be 16

--- simulation_benchmark_test.m
+++ simulation_benchmark_test.m
@@ -8,8 +8,8 @@ clear
 % load outdoorMediumMap
 % load outdoorHardMap
 % load indoorEasyMap
-load indoorMediumMap
-% load indoorHardMap
+% load indoorMediumMap
+load indoorHardMap
 
 % Inflate map
 inflated_map = copy(map);
@@ -26,7 +26,7 @@ Search_planner = OfflineSearchPlanner(inflated_map);
 Test_suite = SearchTestSuite(map);
 
 % Starting Positions
-Search_robot.pose = [2; 15; pi/2];  % [x, y, theta]
+Search_robot.pose = [3; 2; pi/2];  % [x, y, theta]
 opi = [0.25, 0.25, 1];  % [x, y, label]
 
 %% Plan Search Path and Measure Computation Time
