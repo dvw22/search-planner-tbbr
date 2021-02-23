a6f229ce2939ea83121f9465b2606aee5a816e50
Daniel van Wensveen <dvw22@bath.ac.uk>
2021-02-23 18:34:40 +0000

    Switch for map selection added

--- simulation_benchmark_test.m
+++ simulation_benchmark_test.m
@@ -1,15 +1,50 @@
 clear
 
 %% Map Setup
-% load exampleMap;
-% load complexMap;
-% load lidarMap;
-% load outdoorEasyMap
-% load outdoorMediumMap
-% load outdoorHardMap
-% load indoorEasyMap
-% load indoorMediumMap
-load occupancy_maps/indoorHardMap
+while(true)
+    % Prompt user
+    fprintf(['Select map (press number and enter):\n', ...
+             '1: MATLAB example map\n', ...
+             '2: MATLAB complex map\n', ...
+             '3: Indoor easy map\n', ...
+             '4: Indoor medium map\n', ...
+             '5: Indoor hard map\n', ...
+             '6: Outdoor easy map\n', ...
+             '7: Outdoor medium map\n', ...
+             '8: Outdoor hard map\n'])
+    selection = input('Selection: ','s');
+    selection = str2double(selection);
+    
+    % Process selection
+    switch selection
+        case 1
+            load occupancy_maps/exampleMap;
+            break
+        case 2
+            load occupancy_maps/complexMap;
+            break
+        case 3
+            load occupancy_maps/indoorEasyMap;
+            break
+        case 4
+            load occupancy_maps/indoorMediumMap;
+            break
+        case 5
+            load occupancy_maps/indoorHardMap;
+            break
+        case 6
+            load occupancy_maps/outdoorEasyMap;
+            break
+        case 7
+            load occupancy_maps/outdoorMediumMap;
+            break
+        case 8
+            load occupancy_maps/outdoorHardMap;
+            break
+        otherwise
+            disp('Invalid option. Please enter a single integer from 1-8')
+    end
+end
 
 % Inflate map
 inflated_map = copy(map);
@@ -36,7 +71,7 @@ Test_suite.computation_time = toc;
 
 %% Visualisation
 % Plot Path
-% Search_planner.plot_search_path()
+Search_planner.plot_search_path()
 
 % Simulate Search
 result = simulate_offline_search(Search_robot,Test_suite,Search_planner,opi);
